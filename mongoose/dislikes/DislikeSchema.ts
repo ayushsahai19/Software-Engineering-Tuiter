3ef31b963ddc6f5304741a39134aeb42641dcf5d
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-01 19:50:22 -0400

    added schema for dislikes and model for dislikes

---
+++ mongoose/dislikes/DislikeSchema.ts
@@ -0,0 +1,11 @@
+/**
+ * Implements mongoose schema to represent dislikes.
+ */
+import mongoose, {Schema} from "mongoose";
+import Dislike from "../../models/dislikes/Dislike";
+
+const DislikeSchema = new mongoose.Schema<Dislike>({
+    tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
+    dislikedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+}, {collection: "dislikes"});
+export default DislikeSchema;
