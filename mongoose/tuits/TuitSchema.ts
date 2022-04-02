d56c42d484d65d189771632d9a0c251489c3e13a
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-02 00:37:17 -0400

    Added dislikes to tuit schema

--- mongoose/tuits/TuitSchema.ts
+++ mongoose/tuits/TuitSchema.ts
@@ -1,17 +1,15 @@
-import mongoose, {Schema} from "mongoose";
-import Tuit from "../../models/tuits/Tuit";
+import mongoose from "mongoose";
+import Tuit from "../../models/tuits/tuit";
 const TuitSchema = new mongoose.Schema<Tuit>({
     tuit: {type: String, required: true},
-    postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    postedBy: {type: mongoose.Schema.Types.ObjectId, ref: "UserModel", required: true},
     postedOn: {type: Date, default: Date.now},
-    image: String,
-    youtube: String,
-    avatarLogo: String,
-    imageOverlay: String,
     stats: {
         replies: {type: Number, default: 0},
         retuits: {type: Number, default: 0},
-        likes: {type: Number, default: 0}
+        likes: {type: Number, default: 0},
+        dislikes: {type: Number, default: 0}
     }
-}, {collection: "tuits"});
+}, {collection: 'tuits'});
+
 export default TuitSchema;
