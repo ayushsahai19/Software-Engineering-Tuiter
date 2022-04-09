407ea7cecbdce2e18c4ee1668d922075bff6f7e6
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-08 23:56:45 -0400

    Updated Schema for Bookmarks

---
+++ mongoose/bookmarks/BookmarkSchema.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Implements mongoose schema to CRUD
+ * documents in the bookmark collection
+ */
+import mongoose, {Schema} from "mongoose";
+import Bookmark from "../../models/bookmarks/Bookmark";
+
+const BookmarkSchema = new mongoose.Schema<Bookmark>({
+    bookmarkedBy: {type: Schema.Types.ObjectId, ref:"UserModel"},
+    bookmarkedTuit: {type: Schema.Types.ObjectId, ref:"TuitModel"}
+}, {collection: "bookmarks"});
+export default BookmarkSchema;
